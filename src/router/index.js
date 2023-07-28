2e5a17f2aebdb0950892306efc9253a604245bd7
Roscow Lin <roscowlin@Roscows-Air.fios-router.home>
2023-07-27 20:44:34 -0400

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/aboutOriginal',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/aboutView.vue')
+    },
+    {
+      path: '/about1',
+      name: 'about1',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/aboutView1.vue')
+    },
+  ]
+})
+
+export default router
---
+++ src/views/aboutView1.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+<template>
+  <div class="about1">
+    <!-- <h1>This is an about1 page</h1> -->
+    <TheWelcome />
+  </div>
+</template>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about1 {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
