7c254e0828a5c60f30288f68dcff9d334416cedb
Roscow Lin <roscowlin@Roscows-Air.fios-router.home>
2023-08-09 15:58:09 -0400

    main.js adjust

--- src/main.js
+++ src/main.js
@@ -7,6 +7,15 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 const app = createApp(App)
 
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
+
+
 app.use(router)
 
 app.mount('#app')
